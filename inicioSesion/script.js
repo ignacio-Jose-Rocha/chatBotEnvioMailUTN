cc208dbe8916f74c41b807e45fa61eea323916a3
IGNACIO JOSE ROCHA <ignac1997nacho@gmail.com>
2024-04-27 17:54:56 -0300

    agrego el proyecto de chatbot

---
+++ inicioSesion/script.js
@@ -0,0 +1,102 @@
+function obtenerVectores() {
+    let legajos = [10495, 2, 3, 4, 5];
+    let contrasenia = ["ignacio jose rocha", "a b", "c d", "e f", "g h"];
+    let dnis = [40855566, 7, 8, 9, 10, 11];
+    return { legajos, contrasenia, dnis };
+}
+const { legajos, contrasenia, dnis } = obtenerVectores();
+function validarNumero2() {
+    let numero = document.getElementById('exampleInputEmail122').value;
+
+    if (numero.trim() === '') {
+        document.getElementById('error-message').style.display = 'none'; 
+        return true;
+    }
+
+    if (!dnis.includes(parseInt(numero))) { 
+        document.getElementById('error-message').style.display = 'block'; 
+        return false; 
+    } else {
+        document.getElementById('error-message').style.display = 'none'; 
+        mostrarLegajo(parseInt(numero));
+        return true; 
+    }
+}
+
+function validarFormulario() {
+    let nombre = document.getElementById('nombre').value.trim();
+    let identificacion = document.getElementById('exampleInputEmail1').value.trim();
+
+    if (nombre === '' || identificacion === '') {
+        openModal(); 
+        return false;
+    }
+
+    return true;
+}
+
+function openModal() {
+    document.getElementById('myModal').style.display = 'block'; 
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none'; 
+}
+
+
+function mostrarLegajo(numeroDNI) {
+    
+    let index = dnis.indexOf(numeroDNI);
+    let popup = document.getElementById('legajo-message');
+    if (index !== -1) {
+        popup.textContent = 'Su legajo es: ' + legajos[index];
+    } else {
+        popup.textContent = 'No se encontró el legajo correspondiente al DNI ingresado.';
+    }
+    popup.style.display = 'block';
+    setTimeout(function() {
+        popup.style.display = 'none';
+    }, 3000);
+}
+
+
+function validarNombre(){
+    let nombre = document.getElementById('nombre').value;
+    if (nombre.trim() === '') {
+        document.getElementById('error-message').style.display = 'none'; 
+    } else if (/\d/.test(nombre) || nombre.trim() === '0') {
+        document.getElementById('error-message').style.display = 'block'; 
+        return false;
+    } else {
+        document.getElementById('error-message').style.display = 'none'; 
+        return true; 
+    }
+}
+document.getElementById('loader').style.display = 'block';
+window.addEventListener('load', function() {
+            document.getElementById('loader').style.display = 'none';
+        });
+function validarNumero() {
+    let numero = document.getElementById('exampleInputEmail1').value;
+    let nombre = document.getElementById('nombre').value.toLowerCase();
+    
+    if (numero.trim() === '' || isNaN(numero)) {
+        document.getElementById('error-message-legajo').style.display = 'block'; 
+        return false;
+    }
+    
+    let index = legajos.indexOf(parseInt(numero));
+    let index2 = contrasenia.indexOf(nombre);
+    
+    if (index !== -1 && index2 !== -1 && index === index2) {
+        localStorage.setItem('legajo', numero);
+        localStorage.setItem('nombre', nombre);
+        document.getElementById('error-message-legajo').style.display = 'none';
+        document.getElementById('error-message-nombre').style.display = 'none'; 
+        return true;
+    } else {
+        document.getElementById('error-message-legajo').style.display = 'none';
+        document.getElementById('error-message-nombre').style.display = 'block'; 
+        return false;
+    }
+}
---
+++ inicioSesion/script2.js
@@ -0,0 +1,52 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let legajo = localStorage.getItem('legajo');
+    
+    if (!legajo) {
+        window.location.replace("https://chatbot-utn-frsr.vercel.app/");
+    }
+    
+
+    let nombreUsuario = localStorage.getItem('nombre');
+    if (nombreUsuario) {
+        let primerNombre = nombreUsuario.split(' ')[0];
+        document.getElementById('showUsernameBtn').innerText = "Hola, " + primerNombre;
+    }
+
+    document.getElementById('logoutBtn').addEventListener('click', function() {
+       
+        localStorage.removeItem('legajo');
+        localStorage.removeItem('nombre');
+        window.location.href = "https://chatbot-utn-frsr.vercel.app/";
+    }
+);
+document.querySelector('.novedades button').addEventListener('click', function() {
+    if(!legajo){
+        window.location.href = "https://chatbot-utn-frsr.vercel.app/";
+    }
+    else{
+        window.location.href = "materias1.html";
+    }
+    
+});
+document.querySelector('.novedades2 button').addEventListener('click', function() {
+    if(!legajo){
+        window.location.href = "https://chatbot-utn-frsr.vercel.app/";
+    }
+    else{
+        window.location.href = "chatbot/materias2.html";
+    }
+    
+});
+document.querySelector('.novedades3 button').addEventListener('click', function() {
+    if(!legajo){
+        window.location.href = "https://chatbot-utn-frsr.vercel.app/";
+    }
+    else{
+        window.location.href = "novedades.html";
+    }
+    
+});
+
+});
+
